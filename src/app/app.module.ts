50c3e4ef63ffef29c521d67e019df84f5d54e0e5
dabalyan <dabalyan@hotmail.com>
2021-03-15 22:01:11 +0100

    updated packages and configurations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
-import {AppRoutingModule} from './app-routing.module';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {HighchartsChartModule} from 'highcharts-angular';
 import {AppComponent} from './app.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, HighchartsChartModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
